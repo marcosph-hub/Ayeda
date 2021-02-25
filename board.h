d208942a3f50e60a65af6b2f4197b846266cc807
Marcos Padilla Herrera <61996463+marcosph-hub@users.noreply.github.com>
2021-02-25 17:34:12 +0000

    Update board.h

--- board.h
+++ board.h
@@ -1,7 +1,17 @@
-#include "board.h"
-int main () {
-  std::cout << "Hormiga de Langton" << std::endl;
-  Board pruebita;
-  pruebita.Refill();
-  pruebita.Write();
-}
+#include <iostream>
+#include <vector>
+
+class Board {
+  private:
+	std::string **gameboard;
+  int row_size;
+  int column_size;
+
+  public:
+  Board();
+  ~Board();
+  int Get_row_size();
+  int Get_column_size();
+  void Refill();
+  void Write();
+};
